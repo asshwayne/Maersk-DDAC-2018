4230674562eb918e96d254f3c7ffa377b9ae8ab7
asshwayne <asshwayne@gmail.com>
2018-03-21 15:03:46 +0800

    Add project files.

---
+++ MaerskWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MaerskWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
